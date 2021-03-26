7f7ecc54fc325c005cbc9333d0a10cc2048079ec
tpreston96 <tpreston96@gmail.com>
2021-03-26 13:50:27 -0500

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {}; // <-- character map object
+  let max = 0; // <-- variable used to find most common
+  let maxChar = ""; // <-- end result
+
+  //   character map
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  //   iterate charMap to find most frequently used value
+  //   'for in' syntax (ES2015) because charMap is an object
+  for (let char in str) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  //!     Don't forget return
+  return maxChar;
+}
 
 module.exports = maxChar;
