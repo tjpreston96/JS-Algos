64095b15149774ef10981f59ceadf18be05a6d3b
tpreston96 <tpreston96@gmail.com>
2021-04-12 22:47:36 -0500

    merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -19,7 +19,17 @@ function bubbleSort(arr) {
 
 function selectionSort(arr) {}
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+    if (arr.length === 1) {
+      return arr;
+    }
+  
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);
+    const right = arr.slice(center);
+  
+    return merge(mergeSort(left), mergeSort(right));
+  }
 
 function merge(left, right) {}
 
