7be582d9d73a572edb05d18ac62c99081775eac4
tpreston96 <tpreston96@gmail.com>
2021-04-13 22:01:02 -0500

    selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -17,19 +17,37 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+
+  return arr;
+}
 
 function mergeSort(arr) {
-    if (arr.length === 1) {
-      return arr;
-    }
-  
-    const center = Math.floor(arr.length / 2);
-    const left = arr.slice(0, center);
-    const right = arr.slice(center);
-  
-    return merge(mergeSort(left), mergeSort(right));
+  if (arr.length === 1) {
+    return arr;
   }
+
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
 
 function merge(left, right) {}
 
