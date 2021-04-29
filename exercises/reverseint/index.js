121296005454cd685e1ace17ff51575c60bbe695
tpreston96 <tpreston96@gmail.com>
2021-04-29 08:17:14 -0500

    error fix

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -11,7 +11,7 @@
 function reverseInt(n) {
   const reversed = n.toString().split("").reverse().join("");
 
-  if (n > 0) {
+  if (n < 0) {
     return parseInt(reversed) * -1;
   }
   return parseInt(reversed);
